98b7c806292a606443f0fcb17c52ca056722fcc3
Sanjalionething <sanjalisahu@onething.design>
2024-11-28 11:08:02 +0530

    changed star svgs

--- src/Components/Description/Description.js
+++ src/Components/Description/Description.js
@@ -1,43 +1,53 @@
-import React,{useState,useEffect,useRef} from 'react';
-import './description.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./description.css";
 const Description = () => {
-    const [scrollPosition, setScrollPosition] = useState(0);
-    useEffect(() => {
-        const handleScroll = () => {
-          setScrollPosition(window.scrollY);
-        };
-    
-        // Attach the event listener when the component mounts
-        window.addEventListener('scroll', handleScroll);
-    
-        // Detach the event listener when the component unmounts
-        return () => {
-          window.removeEventListener('scroll', handleScroll);
-        };
-      }, []); // Empty dependency array ensures the effect runs only once on mount
-    
-      // Calculate the rotation angle based on the scroll position
-      const rotationAngle = scrollPosition / 2;
-    
-      // Style object with the rotation transformation
-      const imageStyle = {
-        // width: '200px', // Adjust image size as needed
-        transition: 'transform 0.5s ease', // Smooth transition for rotation
-        transform: `rotate(${rotationAngle}deg)`,
-      };
-      
-    return(
-        <section className='lg:py-0 md:py-8 py-8 lg:justify-unset md:justify-unset lg:items-unset md:items-unset justify-center items-center flex w-full gap-20'>
-         <div className='w-1/2 lg:flex md:hidden hidden items-center justify-center'>
-<img  style={imageStyle} className='w-20' src='https://www.antarabhargava.com/icons/star.svg'/>
-         </div>
-         <div className='descriptive-content lg:w-1/2 md:w-full w-11/12 text-white font-syne'>
-            <h4 className='lg:text-left  md:text-center text-center font-semibold lg:mb-4 md:mb-4 mb-4'>I'm a <span className='gradient'>Software Developer</span> with 1+ years of hands-on experience in developing and implementing innovative solutions.</h4>
-            <p className='lg:text-left md:text-center text-center'>I'm passionate about creating efficient and scalable applications. I’ve had the privilege to work on a diverse range of projects across industries ranging from e-commerce, education, health and real estate. Proficient in front-end and back-end technologies, dedicated to delivering high-quality code and exceptional user experiences. I've collaborated with cross-functional teams to deliver finest software solutions.</p>
-         </div>
-        </section>
-       
+  const [scrollPosition, setScrollPosition] = useState(0);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
 
-    )
-}
+    // Attach the event listener when the component mounts
+    window.addEventListener("scroll", handleScroll);
+
+    // Detach the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
+  // Calculate the rotation angle based on the scroll position
+  const rotationAngle = scrollPosition / 2;
+
+  // Style object with the rotation transformation
+  const imageStyle = {
+    // width: '200px', // Adjust image size as needed
+    transition: "transform 0.5s ease", // Smooth transition for rotation
+    transform: `rotate(${rotationAngle}deg)`,
+  };
+
+  return (
+    <section className="lg:py-0 md:py-8 py-8 lg:justify-unset md:justify-unset lg:items-unset md:items-unset justify-center items-center flex w-full gap-20">
+      <div className="w-1/2 lg:flex md:hidden hidden items-center justify-center">
+        <img style={imageStyle} className="w-20" src="/star.svg" />
+      </div>
+      <div className="descriptive-content lg:w-1/2 md:w-full w-11/12 text-white font-syne">
+        <h4 className="lg:text-left  md:text-center text-center font-semibold lg:mb-4 md:mb-4 mb-4">
+          I'm a <span className="gradient">Software Developer</span> with 1+
+          years of hands-on experience in developing and implementing innovative
+          solutions.
+        </h4>
+        <p className="lg:text-left md:text-center text-center">
+          I'm passionate about creating efficient and scalable applications.
+          I’ve had the privilege to work on a diverse range of projects across
+          industries ranging from e-commerce, education, health and real estate.
+          Proficient in front-end and back-end technologies, dedicated to
+          delivering high-quality code and exceptional user experiences. I've
+          collaborated with cross-functional teams to deliver finest software
+          solutions.
+        </p>
+      </div>
+    </section>
+  );
+};
 export default Description;
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './hero.css'
+import React from "react";
+import "./hero.css";
 const Hero = () => {
-    return (
-      <section className='md:mb-8 lg:mt-0 md:mt-0 mt-8 lg:mb-0 hero-section flex flex-col align-center flex-column'>
-        {/* <div className='flex justify-between pr-8'>
+  return (
+    <section className="md:mb-8 lg:mt-0 md:mt-0 mt-8 lg:mb-0 hero-section flex flex-col align-center flex-column">
+      {/* <div className='flex justify-between pr-8'>
         <button className='flex'>
         <a href="/" class="bttn bttn-primary"> <span className='flex items-center gap-4'><img className="h-7 w-7 rounded-full" src={profilePic}/><p className='font-syne'>SANJALI SAHU</p>
                     </span>
@@ -14,13 +14,24 @@ const Hero = () => {
             <span className=''>Work</span>
           </div>
           </div> */}
-          {console.log(window.innerWidth)}
-       <h2 className='relative pl-4 pt-4 lg:pl-24 md:pt-8 md:pl-0 md:text-7xl font-russo lg:pt-heroh1 md:pt-0 align-center text-4xl lg:text-8xl flex flex-col lg:gap-20 gap-4 md:gap-12 text-white leading-10'><span>Coding dreams</span> <span className='lg:ml-56 md:ml-20 md:whitespace-nowrap'>into digital reality.</span>
-      <img className="absolute lg:top-60 lg:left-52 h-20 md:w-fourRem md:top-36 md:left-4 lg:w-20 w-8 top-[-50%]" src="https://www.antarabhargava.com/icons/star.svg"/>
-        </h2>
-        <span className='opac-text text-white w-72 lg:mx-24 md:mx-0 md:mt-12 lg:mt-24 lg:mb-12 lg:ml-28 ml-4 mt-4  font-syne  font-normal'>Passionate about code craftsmanship, problem-solving, and shaping seamless digital experiences. Currently leading the way as a Fullstack Developer.</span>
-      </section>
-    );
-  };
-  
-  export default Hero;
+      {console.log(window.innerWidth)}
+      <h2 className="relative pl-4 pt-4 lg:pl-24 md:pt-8 md:pl-0 md:text-7xl font-russo lg:pt-heroh1 md:pt-0 align-center text-4xl lg:text-8xl flex flex-col lg:gap-20 gap-4 md:gap-12 text-white leading-10">
+        <span>Coding dreams</span>{" "}
+        <span className="lg:ml-56 md:ml-20 md:whitespace-nowrap">
+          into digital reality.
+        </span>
+        <img
+          className="absolute lg:top-60 lg:left-52 h-20 md:w-fourRem md:top-36 md:left-4 lg:w-20 w-8 top-[-50%]"
+          src="/star.svg"
+        />
+      </h2>
+      <span className="opac-text text-white w-72 lg:mx-24 md:mx-0 md:mt-12 lg:mt-24 lg:mb-12 lg:ml-28 ml-4 mt-4  font-syne  font-normal">
+        Passionate about code craftsmanship, problem-solving, and shaping
+        seamless digital experiences. Currently leading the way as a Fullstack
+        Developer.
+      </span>
+    </section>
+  );
+};
+
+export default Hero;
